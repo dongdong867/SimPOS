d81a2d8be77823af1436cf60170f6bc073e40dbe
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-08 07:23:43 +0800

    refactor(OrdersList): break view into small blocs

--- Views/Order/OrdersList.swift
+++ Views/Order/OrdersList.swift
@@ -17,56 +17,81 @@ struct OrdersList: View {
                 LazyVStack {
                     ForEach(orders) { order in
                         Divider()
-                        VStack(alignment: .leading, spacing: 8) {
-                            HStack {
-                                Text("No. \(order.orderNumber)")
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                if(order.finished) {
-                                    Image(systemName: "checkmark.circle.fill")
-                                        .foregroundStyle(.green)
-                                }
-                                
-                            }
-                            VStack {
-                                ForEach(order.orderProducts) { item in
-                                    HStack {
-                                        HStack {
-                                            Text(item.product.name)
-                                            Text("X\(item.amount)")
-                                        }
-                                        Spacer()
-                                        Text(
-                                            item.product.price * Float(item.amount),
-                                            format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                                        )
-                                    }
-                                }
-                                Divider()
-                            }
-                            .padding(.leading, 8)
-                            if(!order.note.isEmpty) {
-                                Text(order.note)
-                                    .font(.callout)
-                                    .foregroundStyle(.gray)
-                            }
-                            HStack {
-                                Text("Subtotal")
-                                Spacer()
-                                Text(order.subtotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            }
-                            .font(.headline)
-                            Text(order.createTime, format: .dateTime)
-                                .font(.footnote)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .padding(8)
+                        orderListItem(order)
                     }
                 }
                 .padding(.horizontal)
                 .navigationTitle("Orders")
             }
         }
+    }
+    
+    func orderListItem(_ order: Order) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+            orderTitle(order)
+            productList(order)
+            if(!order.note.isEmpty){
+                notes(order)
+            }
+            subtotal(order)
+            createTime(order)
+        }
+        .padding(8)
+    }
+    
+    func orderTitle(_ order: Order) -> some View {
+        HStack {
+            Text("No. \(order.orderNumber)")
+                .font(.title2)
+                .fontWeight(.semibold)
+            if(order.finished) {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundStyle(.green)
+            }
+            
+        }
+    }
+    
+    func productList(_ order: Order) -> some View {
+        VStack {
+            ForEach(order.orderProducts) { item in
+                HStack {
+                    HStack {
+                        Text(item.product.name)
+                        Text("X\(item.amount)")
+                    }
+                    Spacer()
+                    Text(
+                        item.product.price * Float(item.amount),
+                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                }
+            }
+            Divider()
+        }
+        .padding(.leading, 8)
+    }
+    
+    func notes(_ order: Order) -> some View {
+        Text(order.note)
+            .font(.callout)
+            .foregroundStyle(.gray)
+    }
+    
+    func subtotal(_ order: Order) -> some View {
+        HStack {
+            Text("Subtotal")
+            Spacer()
+            Text(order.subtotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        }
+        .font(.headline)
+
+    }
+    
+    func createTime(_ order: Order) -> some View {
+        Text(order.createTime, format: .dateTime)
+            .font(.footnote)
+            .frame(maxWidth: .infinity, alignment: .trailing)
     }
 }
 
