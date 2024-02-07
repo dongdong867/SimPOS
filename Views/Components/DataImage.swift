35d9709f74c03a86c576bb3cccd493b6386f83d0
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-08 06:38:04 +0800

    fix(DataImage): fix image display with wrong width

--- Views/Components/DataImage.swift
+++ Views/Components/DataImage.swift
@@ -13,9 +13,13 @@ struct DataImage: View {
     var body: some View {
         if let data = data,
            let uiImage = UIImage(data: data) {
-            Image(uiImage: uiImage)
-                .resizable()
-                .aspectRatio(1, contentMode: .fill)
+            Rectangle()
+                .aspectRatio(1, contentMode: .fit)
+                .overlay(
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                )
                 .clipShape(RoundedRectangle(cornerRadius: 8))
         } else {
             RoundedRectangle(cornerRadius: 8)
