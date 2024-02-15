277f41af2c40e14beeeb1a9c3ad4a00f7368ccdf
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-16 05:16:26 +0800

    feat(ProductList): create view model for products view

---
+++ Views/Product/ProductList.swift
@@ -0,0 +1,35 @@
+//
+//  ProductList.swift
+//  
+//
+//  Created by Dong on 2024/2/13.
+//
+
+import SwiftData
+import SwiftUI
+
+final class ProductList: ObservableObject {
+    @Published var products = [Product]()
+    
+    var modelContext: ModelContext
+    
+    init(modelContext: ModelContext) {
+        self.modelContext = modelContext
+        fetchProducts(with: "")
+    }
+    
+    func fetchProducts(with query: String) {
+        let descriptor = FetchDescriptor<Product> (predicate: #Predicate {
+            if(query.isEmpty) { true }
+            else {
+                $0.name.localizedStandardContains(query)
+            }
+        })
+
+        products = (try? modelContext.fetch(descriptor)) ?? []
+    }
+    
+    func createProduct(_ product: Product) {
+        modelContext.insert(product)
+    }
+}
