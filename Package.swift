61097b707c6e40f33231f82adb98134923ca2203
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-21 04:19:22 +0800

    feat(Package): add camera access request to file

--- Package.swift
+++ Package.swift
@@ -31,6 +31,9 @@ let package = Package(
                 .landscapeRight,
                 .landscapeLeft,
                 .portraitUpsideDown(.when(deviceFamilies: [.pad]))
+            ],
+            capabilities: [
+                .camera(purposeString: "For scanning product barcode.")
             ]
         )
     ],
